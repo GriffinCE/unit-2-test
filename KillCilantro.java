38e20fb7c4053dbda3cefd5b7cbe48072ea29b52
GriffinCE <griffinedson@gmail.com>
2025-12-12 09:32:40 -0800

    test one

--- KillCilantro.java
+++ KillCilantro.java
@@ -1,5 +1,20 @@
 public class KillCilantro {
    public static void main(String[] args) {
-      
+      System.out.print(killCilantro("cilantroveadcolantrocilantrow"));
+   }
+   public static String killCilantro(String input) {
+      String recipe = input + " ";
+      String newRecipe = "";
+      String string1 = "";
+      for (int i = 0; i <= input.length(); i++) {
+         if (i < recipe.length() - 8 && recipe.substring(i, i + 8).equals("cilantro")) {
+            newRecipe += "parsley";
+            i += 7;
+         } else {
+            newRecipe += recipe.substring(i, i + 1);
+
+         }
+      }
+      return newRecipe;
    }
 }
